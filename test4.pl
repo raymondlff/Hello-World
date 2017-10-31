835ba14608a0ea2faeb04bbc264146a1bbafe454
Raymond <30335828+raymondlff@users.noreply.github.com>
2017-10-31 17:10:42 +0800

    Add files via upload

---
+++ test4.pl
@@ -0,0 +1,10 @@
+#! /usr/bin/perl
+
+$f_a = 493827155/1234567888;
+$f_b = 493827069/1234567673;
+
+if($f_a < $f_b){
+    print "493827155/1234567888 is smaller\n";
+}
+
+printf ('two number: %e, %e.\n', $f_a, $f_b);
